5e662082ff7897dc3d59eb58884eaea6d00b3540
CVOvidiu <cvovidiu2002@gmail.com>
2022-11-17 11:04:39 +0200

    Module System: Second challenge

---
+++ Module System/Challenge2/challenge2.js
@@ -0,0 +1,12 @@
+/*
+    Challenge: Define and use a function in a new file
+
+    1. Create a new file
+    2. Create getNotes function that returns "Your notes..."
+    3. Export getNotes function
+    4. From this file, load in and call the function printing message to console
+*/
+
+const getNotes = require('./script.js');
+
+getNotes();
