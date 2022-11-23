ac5254e087ac9b9c3b1d4abc9c5f44cd98c94f0c
CVOvidiu <cvovidiu2002@gmail.com>
2022-11-23 18:22:52 +0200

    Weather App: refactored some code

--- Weather-App/app.js
+++ Weather-App/app.js
@@ -1,27 +1,19 @@
 const positionstack = require(`./utils/positionstack.js`);
 const forecast = require('./utils/forecast.js');
 
-/*
-    Goal: Accept location via command line argument
-    1. Access the command line argument without yargs
-    2. Use the string value as the input for positionstack
-    3. Only positionstack if a location was provided
-    4. Test your work with a couple locations
-*/
-
 if(process.argv[2]) {
-    positionstack(process.argv[2], (error, data) => {
+    positionstack(process.argv[2], (error, {latitude, longitude, location} = {}) => { // When positionstack throws an error, we would destructure 'undefined', so that's why '= {}', it sets an empty object as default
         if(error)
             return console.log(error);
         
-        forecast(data.latitude, data.longitude, (error, forecastData) => {
+        forecast(latitude, longitude, (error, forecastData) => {
             if(error)
                 return console.log(error);
     
-            console.log(data.location);
+            console.log(location);
             console.log(forecastData);
         })
     });
 } else {
-    console.log('Error: Enter a valid location!')
+    console.log('Error: Enter a valid location!');
 }
--- Weather-App/utils/forecast.js
+++ Weather-App/utils/forecast.js
@@ -3,7 +3,7 @@ const request = require('request');
 const forecast = (latitude, longitude, callback) => {
     const url = `http://api.weatherstack.com/current?access_key=f7725b2e0cd11286714dca6f854023ca&query=${latitude},${longitude}`;
 
-    request({url: url, json: true}, (err, res) => {
+    request({url, json: true}, (err, res) => {
         if(err)
             callback(`Oops! Wrong on your end.`, undefined);
         else if(res.success === false)
