ebe9d2d5213132b9cb06fdac06e0be76672a3dff
CVOvidiu <cvovidiu2002@gmail.com>
2022-11-23 13:58:36 +0200

    HTTP Request: Challege 1 completed

---
+++ Weather-App/app.js
@@ -0,0 +1,10 @@
+const request = require('request');
+
+const url = 'http://api.weatherstack.com/current?access_key=yourKey&query=44.4268,26.1025';
+
+request({url: url, json: true}, (err, res) => {
+    const temperature = res.body.current.temperature;
+    const feelsLike = res.body.current.feelslike;
+    
+    console.log(`It is currently ${temperature} degrees out. It feels like ${feelsLike} degrees out.`);
+});
