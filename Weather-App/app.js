6fae5a07121b005c9578b465367552aa7dbdb775
CVOvidiu <cvovidiu2002@gmail.com>
2022-11-23 17:10:11 +0200

    Weather App: weatherstack API integration

--- Weather-App/app.js
+++ Weather-App/app.js
@@ -1,7 +1,22 @@
-const request = require('request');
 const positionstack = require(`./utils/positionstack.js`);
+const forecast = require('./utils/forecast.js');
 
 positionstack('Bucharest', (error, data) => { // Pass the address to position stack and get error/data and do something
     console.log(`Error: ${error}`);
     console.log(`Data: `, data);
 });
+
+//
+// Goal: Create a reusable function for getting the forecast
+//
+// 1. Setup the "forecast" function in utils/forecast.js
+// 2. Require the function in app.js and call it as shown below
+// 3. The forecast function should have three potential calls to callback:
+//    - Low level error, pass string for error
+//    - Coordinate error, pass string for error
+//    - Success, pass forecast string for data (same format as from before)
+
+forecast(44.4268, 26.1025, (error, data) => {
+    console.log('Error', error)
+    console.log('Data', data)
+  })
---
+++ Weather-App/utils/forecast.js
@@ -0,0 +1,22 @@
+const request = require('request');
+
+const forecast = (latitude, longitude, callback) => {
+    const url = `http://api.weatherstack.com/current?access_key=f7725b2e0cd11286714dca6f854023ca&query=${latitude},${longitude}`;
+
+    request({url: url, json: true}, (err, res) => {
+        if(err)
+            callback(`Oops! Wrong on your end.`, undefined);
+        else if(res.success === false)
+            callback(`Request error.`, undefined)
+        else {
+            const temperature = res.body.current.temperature;
+            const feelsLike = res.body.current.feelslike;
+            callback(undefined, {
+                temperature: temperature,
+                feelsLike: feelsLike
+            });
+        }
+    })
+};
+
+module.exports = forecast;
